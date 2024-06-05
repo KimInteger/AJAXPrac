8e6afd421b4924f99c4a4d1ed6e80099e0f23c2a
KimInteger <kjs20151240@gmail.com>
2024-06-05 11:24:26 +0900

    xhr에 eventListener세팅

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('와 script.js');
+const getPokemonAJAX = () => {
+  const xhr = new XMLHttpRequest();
+  // * xhr이란 변수(상수)에 생성자 함수 instant가 XMLHttpRequest인 규격의 객체를 담는다.
+  console.dir("데이터 받기 전 조회 : ", + xhr);
+  xhr.open("GET","https://pokeapi.co/api/v2/pokemon/",true);
+  xhr.addEventListener('load',()=>{
+    if (xhr.status === 200) {
+
+    }
+  });
+};
